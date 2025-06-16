05170b84d19a4a503ad1c84ac55bbaa0d6f3a9b6
GeorgesMoulux <matteomng@outlook.fr>
2025-06-16 08:55:45 +0000

    llll

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - return void
+ * Description: affiche la phrase
+ * Return: 0; succ√s
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
