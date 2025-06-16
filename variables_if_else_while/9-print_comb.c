497eccd999a1563214a445a66aa6f40860c41f42
GeorgesMoulux <matteomng@outlook.fr>
2025-06-16 13:28:54 +0000

    kkkkkkkkkkkkkkkkk

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - return void
+ * Description: affiche la phrase
+ * Return: 0; succs
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+		if (n < 9)
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
