f863fbef402514d3f0ee095fd499b19435a5ed3f
GeorgesMoulux <matteomng@outlook.fr>
2025-06-16 12:01:19 +0000

    ze

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - return void
+ * Description: affiche la phrase
+ * Return: 0; succs
+ */
+
+int main(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	for (alph = 'A'; alph <= 'Z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
