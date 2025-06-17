c8cb697b07e70d2d6979bcbeefae08eb66814009
GeorgesMoulux <matteomng@outlook.fr>
2025-06-17 11:08:06 +0000

    you

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet- id tggtrgtrgt
+ * Description: affiche [a - z]
+ * Return: always 0;
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	_putchar(a);
+	_putchar('\n');
+}
