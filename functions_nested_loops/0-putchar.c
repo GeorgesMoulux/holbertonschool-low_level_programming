7649038e47e9b1347a8c1cbb450279f49c100a4e
GeorgesMoulux <matteomng@outlook.fr>
2025-06-17 10:10:11 +0000

    jk

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,32 +3,12 @@
 #include "main.h"
 
 /**
- * main- return void
- * Description: lol
- * Return: 0 succs
+ * main - return void
+ * Description: affiche la phrase
+ * Return: 0; succs
  */
 
-/*void  putstring(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-}
-
 int main(void)
-{
-	char *str = "_putchar\n";
-
-	putstring(str);
-	return (0);
-}*/
-
-int main()
 {
 	_putchar('_');
 	_putchar('p');
