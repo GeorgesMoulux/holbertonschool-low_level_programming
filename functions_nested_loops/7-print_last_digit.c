ea18cfcff8c547c6ece9d9c0de789cca1ef04531
GeorgesMoulux <matteomng@outlook.fr>
2025-06-17 14:03:02 +0000

    ll

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print le dernier chiffre dun nb
+ * Description: a
+ * Return: 0; succs
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n %  10;
+	if (last < 0)
+		last = -last;
+
+	_putchar('0' + last);
+	return (last);
+}
