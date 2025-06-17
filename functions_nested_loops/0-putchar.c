5af6eef7e759092d8ad0b248c05c06d812c795d0
GeorgesMoulux <matteomng@outlook.fr>
2025-06-17 09:32:04 +0000

    llll

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - return void
+ * Description: print putchar
+ * Return: 0; succs
+ */
+void  putstring(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int main(void)
+{
+	char *str = "_putchar";
+
+	putstring(str);
+	return (0);
+}
