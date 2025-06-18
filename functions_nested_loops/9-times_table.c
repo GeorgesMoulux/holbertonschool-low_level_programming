2a1f46a513b7c68abdbd96d4a18654cc0fa312ae
GeorgesMoulux <matteomng@outlook.fr>
2025-06-18 11:07:18 +0000

    zer

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/*
+ * times_table- table de multiplication
+ */
+
+void times_table(void)
+{
+	int n;
+	int m;
+	int p;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			p = n * m;
+			if (p < 10)
+			{
+				_putchar(p + '0');
+				if (m != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			if (p >= 10)
+			{
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+				if (m != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
