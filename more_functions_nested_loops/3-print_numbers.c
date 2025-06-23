ab5fccfac0de276b09f2db81d62cb9d2ffc26570
GeorgesMoulux <matteomng@outlook.fr>
2025-06-23 08:53:03 +0000

    re

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - if uppercase
+ *
+ * Description: a
+ * Return: 0; succs
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+		i++;
+	}
+}
