425cad1a8974873df3dd70083bc4e3eab0ef25f7
GeorgesMoulux <matteomng@outlook.fr>
2025-06-23 11:56:31 +0000

    llll

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - replace by fizz or buzz
+ */
+
+void fizz_buzz(void)
+{
+	int n;
+	char a[] = "Fizz";
+	char b[] = "Buzz";
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("%s ", a);
+		else if (n % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", n);
+	}
+}
+
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
