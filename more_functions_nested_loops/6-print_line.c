287dad9ff9142c8615f5c14c513cd0f1bfc13ff2
GeorgesMoulux <matteomng@outlook.fr>
2025-06-23 10:03:37 +0000

    ppo

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *@n: num
+ * Description: a
+ * Return: 0; succs
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
