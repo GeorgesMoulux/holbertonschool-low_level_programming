894639fa72e40c2a6b48e3d8293dc1ee608d6aa9
GeorgesMoulux <matteomng@outlook.fr>
2025-06-25 07:56:08 +0000

    zerd

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
+/**
+ * print_array - print tab
+ * @a: pointeur sur le debut de la chaine
+ * @n: taille du tableau
+ * Description: nul
+ * Return: 0, ded
+ */
+
 void print_array(int *a, int n)
 {
 	int *p = 0;
 
 	for (p = a; p < a + n; p++)
 	{
-    		printf("%d, ", *p);
+		if (p != a + n - 1)
+			printf("%d, ", *p);
+
+		else
+			printf("%d\n", *p);
 	}
 }
