151f036d6b2e07e6a71be580faedb046897f9ec9
GeorgesMoulux <matteomng@outlook.fr>
2025-06-25 12:33:55 +0000

    first try ez

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - ascii to intger
+ * @s: string a transforme
+ * Description: nul
+ * Return: nul
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int m = 0; /*compteur de signe moins */
+	int n = 0;
+
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			m++;
+
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		n = n * 10;
+		n += s[i] - 48;
+		i++;
+	}
+	if (m % 2 != 0)
+		n = -n;
+	return (n);
+}
