9269a7ea58f677eccc7f367266b778feaf8d243d
GeorgesMoulux <matteomng@outlook.fr>
2025-06-25 08:16:43 +0000

    zaz

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - src into dest
+ * @dest: pointeur sur le debut de la chaine de dest
+ * @src: pointeur sur le debut de la chaine de src
+ * Description: nul
+ * Return: 0, ded
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
