9740391263720011b964e18f7c9e0eafaeb8b1f2
GeorgesMoulux <matteomng@outlook.fr>
2025-06-25 13:53:29 +0000

    szer

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - n char de src
+ * @dest: dest
+ * @src: src
+ * @n: nb de src
+ * Description: nul
+ * Return: nul
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int n = (_strlen(dest));
+
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
