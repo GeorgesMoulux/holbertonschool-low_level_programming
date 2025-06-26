4ddcf0f5aab856905870eb83c0e839a94adf62d0
GeorgesMoulux <matteomng@outlook.fr>
2025-06-26 13:42:49 +0000

    zerrroororo

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - cap words
+ * @str: str
+ * Return: new str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i - 1] == ' ' || str[i - 1] == '\t'
+				|| str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+|| str[i - 1] == '.' ||
+str[i - 1] == '!' || str[i - 1] == '?'
+|| str[i - 1] == '"' ||
+str[i - 1] == '(' || str[i - 1] == ')'
+|| str[i - 1] == '{' ||
+str[i - 1] == '}')
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
