990dc7e16c22a4d008157f0890c5c1e13b21a5d2
GeorgesMoulux <matteomng@outlook.fr>
2025-06-26 10:59:19 +0000

    lls

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - cpy string with null bytes
+ * @dest: dest
+ * @src: src
+ * @n: size of src
+ * Description: nul
+ * Return: Dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest)
+}
