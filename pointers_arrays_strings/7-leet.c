677e8d6568fad5b8cb4d0ebc0ad96dda80f17787
GeorgesMoulux <matteomng@outlook.fr>
2025-06-27 11:07:14 +0000

    zeroo

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,24 +1,32 @@
 #include "main.h"
 
+/**
+ * leet - swap letters
+ * @str: string
+ * Description: nul
+ * Return: str
+ */
+
 char *leet(char *str)
 {
-	void leet_convert(char *str)
-{
-    char *from = "aAeEoOtTlL";
-    char *to   = "4433007711";
+	char *from = "aAeEoOtTlL";
+	char *to   = "4433007711";
+	int i = 0;
 
-    int i = 0;
-    while (str[i])
-    {
-        int j = 0;
-        while (from[j])
-        {
-            if (str[i] == from[j]) {
-                str[i] = to[j];
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
-}	
+	while (str[i])
+	{
+		int j = 0;
+
+		while (from[j])
+		{
+			if (str[i] == from[j])
+			{
+				str[i] = to[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
