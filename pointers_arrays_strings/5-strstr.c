ae9458cd55643a36c0adb6c8cdd8e974065a69f6
GeorgesMoulux <matteomng@outlook.fr>
2025-07-01 11:13:30 +0000

    zer

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,29 @@
 #include "main.h"
 
+/**
+ * _strstr - find chain
+ * @haystack: str to look in
+ * @needle: str to fin
+ * Description: nul
+ * Return: adresse de premeier occurence de needle
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0;
 	unsigned int j = 0;
 
+	if (needle[j] == '\0')
+		return (0);
 	while (haystack[i])
 	{
 		for (j = 0; needle[j]; j++)
 		{
 			if (haystack[i + j] != needle[j])
 				break;
-			else 
-				return (&haystack[i]);
 		}
+			if (needle[j] == '\0')
+				return (&haystack[i]);
 	i++;
 	}
 	return (0);
---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = 0;
+	
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+		{
+				
