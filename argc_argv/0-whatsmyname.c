1be2c7b2340a6939fde2f3f2180caa31e94c8826
GeorgesMoulux <matteomng@outlook.fr>
2025-07-02 11:59:56 +0000

    zer

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		printf("%s", argv[0]);
+	return (0);
+}
