7937d8de600e0109e4c2240f07e7330c0cd875ca
GeorgesMoulux <matteomng@outlook.fr>
2025-07-04 10:35:47 +0200

    lov

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,10 +3,16 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0;
-	int j = 0;
+	int diag1 = 0;
+	int diag2 = 0;
 	
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-				
+		diag1 += a[i * size + i];
+		diag2 += a[i * size + (size - i - 1)];
+	}
+		printf("%d, ", (diag1));
+		printf("%d", (diag2));
+		printf("\n");
+}
+
