9afc4533324f269c1c6ea1153df1d2f748d7fb0b
GeorgesMoulux <matteomng@outlook.fr>
2025-07-04 13:22:23 +0200

    ll

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int result = 0;
+	int j = 0;
+	int i = 0;
+
+	if (argc < 3)
+		return (printf("0\n"));
+
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				j++;
+				else
+				return (printf("Error\n"));
+			}
+
+		result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
