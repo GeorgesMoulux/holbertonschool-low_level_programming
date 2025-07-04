5817fd50d743be906bc305cc5d7ad2e37134369d
GeorgesMoulux <matteomng@outlook.fr>
2025-07-04 17:43:18 +0200

    lol

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_rev- rev string
- * @s: string
+ * swap_char - swap char
+ * @a: string
+ * @b: ksks
  * Description: nul
  * Return: 0, ded
  */
-
 void swap_char(char *a, char *b)
 {
-        int c;
+	int c;
 
-        c  = *a;
-        *a = *b;
-        *b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
 
 #include "main.h"
@@ -27,13 +27,19 @@ void swap_char(char *a, char *b)
 
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i] != '\0')
-                i++;
-        return (i);
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
+/**
+ * rev_string- rev string
+ * @s: string
+ * Description: nul
+ * Return: 0, ded
+ */
 void rev_string(char *s)
 {
 	int i = 0;
