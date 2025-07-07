a4f475661ad71bea192c56384d8cbebfb8cf9707
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 15:25:48 +0200

    zzed

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,6 +14,12 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i = 0;
 	unsigned int j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	unsigned int t_size = strlen(s1) + strlen(s2);
 	char *buffer;
 
