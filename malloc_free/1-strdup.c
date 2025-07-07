ad9a7a4f4c0c6c1ea1c9c8aace50740e1c2b10f8
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 11:27:41 +0200

    lol

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - dup
+ * @str: string
+ * Description: nul
+ * Return: buffer
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	unsigned int size = strlen(str);
+	char *buffer;
+
+	if (str == NULL)
+		return (NULL);
+
+	buffer = malloc((size + 1) * sizeof(char));
+
+	if (buffer == NULL)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+		buffer[i] = str[i];
+	return (buffer);
+}
