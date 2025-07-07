bfff764f45e760124eda5b0ed92558361533edd2
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 13:40:28 +0200

    lll

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - dup
+ * @str: string
+ * Description: nul
+ * Return: buffer
+ */
+
+ char *str_concat(char *s1, char *s2)
+ {
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int t_size = strlen(s1) + strlen(s2);
+	char *buffer;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	buffer = malloc((t_size +1) * sizeof(char));
+
+	if (buffer == NULL)
+		return (NULL);
+
+	while (s1[i])
+		{
+			buffer[i] = s1[i];
+			i++;
+		}
+	while (s2[j])
+		{
+			buffer[i] = s2[j];
+			j++;
+			i++;
+		}
+	return (buffer);
+ }
