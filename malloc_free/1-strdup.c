ab4af19b2f7e3813f83563e0c91f1ec276161099
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 15:54:22 +0200

    llllll

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,6 +3,22 @@
 #include <string.h>
 
 /**
+ * _strlen - taille string
+ * @s: pointeur
+ * Description: nul
+ * Return: 0, ded
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+	return (i);
+}
+
+/**
  * _strdup - dup
  * @str: string
  * Description: nul
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * _strlen - taille string
+ * @s: pointeur
+ * Description: nul
+ * Return: 0, ded
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+	return (i);
+}
 
 /**
  * str_concat - dup
@@ -14,19 +29,16 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i = 0;
 	unsigned int j = 0;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	unsigned int t_size = strlen(s1) + strlen(s2);
+	unsigned int t_size;
 	char *buffer;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	t_size = _strlen(s1) + _strlen(s2);
+
 	buffer = malloc((t_size + 1) * sizeof(char));
 
 	if (buffer == NULL)
