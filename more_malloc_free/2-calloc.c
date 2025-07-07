87afab3769cd44ea7bf2318adb26cdef1280e34c
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 17:13:21 +0200

    lol

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - check
+ * @nmemb: number of bytes
+ * @size: size of a bytes
+ * Description: NUL
+ * Return: NUL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *buffer;
+	int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	buffer = malloc(nmemb * size);
+	if (buffer == NULL)
+		return (NULL);
+	while (buffer[i])
+	{
+		buffer[i] = 0;
+		i++;
+	}
+	return (buffer);
+}
