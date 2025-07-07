3519694002dbb62f835b2dee02a7e64e7fcc9efe
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 15:06:47 +0200

    zer

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,13 +10,14 @@
  * Return: buffer
  */
 
- void free_grid(int **grid, int height)
+void free_grid(int **grid, int height)
 {
 	int i = 0;
+
 	if (grid == NULL || height <= 0)
 	return;
 
-	for (i = 0; i < height; i++) 
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 	free(grid);
 }
