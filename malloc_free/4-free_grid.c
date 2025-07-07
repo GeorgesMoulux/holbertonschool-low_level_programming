f4714a2d4acfb4fde5f81c6b868b26e875af5b00
GeorgesMoulux <matteomng@outlook.fr>
2025-07-07 15:05:20 +0200

    ll

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_grid - dup
+ * @grid: string
+ * @height: string
+ * Description: nul
+ * Return: buffer
+ */
+
+ void free_grid(int **grid, int height)
+{
+	int i = 0;
+	if (grid == NULL || height <= 0)
+	return;
+
+	for (i = 0; i < height; i++) 
+		free(grid[i]);
+	free(grid);
+}
