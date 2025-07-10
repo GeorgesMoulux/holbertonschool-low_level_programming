8c0f7a2ab51fd7bc78951cee5afc64d375ed2989
GeorgesMoulux <matteomng@outlook.fr>
2025-07-10 15:38:17 +0200

    llllllllllll

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,53 @@
 #include "dog.h"
 #include <stdlib.h>
 
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - taille string
+ * @s: pointeur
+ * Description: nul
+ * Return: 0, ded
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+	return (i);
+}
+
+/**
+ * _strdup - dup
+ * @str: string
+ * Description: nul
+ * Return: buffer
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	unsigned int size = _strlen(str);
+	char *buffer;
+
+	if (str == NULL)
+		return (NULL);
+
+	buffer = malloc((size + 1) * sizeof(char));
+
+	if (buffer == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		buffer[i] = str[i];
+
+	buffer[i] = '\0';
+
+	return (buffer);
+}
+
 /**
  * new_dog - lll
  * @name: dff
@@ -17,9 +64,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (d2 == NULL)
 		return (NULL);
-	d2->name = strdup(name);
+	d2->name = _strdup(name);
 	d2->age = age;
-	d2->owner = strdup(owner);
+	d2->owner = _strdup(owner);
 
 	if (d2->name == NULL ||
 		d2->owner == NULL)
