5311557bff1c2213c12c1f808e3258a4391534e4
GeorgesMoulux <matteomng@outlook.fr>
2025-07-10 15:28:12 +0200

    llll

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,5 +22,6 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
