cfaaf4fe65e8cb98e591d62123a28f8c77420c12
GeorgesMoulux <matteomng@outlook.fr>
2025-07-10 14:56:24 +0200

    ll

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -70,5 +70,4 @@ void free_grid(int **grid, int height);
 struct dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 #endif
