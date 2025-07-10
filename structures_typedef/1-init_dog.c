953593fe94d076f121ba051648f66167b13ed3c9
GeorgesMoulux <matteomng@outlook.fr>
2025-07-10 15:26:06 +0200

    llll

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,48 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - taille string
- * @s: pointeur
- * Description: nul
- * Return: 0, ded
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	i++;
-	return (i);
-}
-
-/**
- * _strdup - dup
- * @str: string
- * Description: nul
- * Return: buffer
- */
-char *_strdup(char *str)
-{
-	unsigned int i = 0;
-	unsigned int size = _strlen(str);
-	char *buffer;
-
-	if (str == NULL)
-		return (NULL);
-
-	buffer = malloc((size + 1) * sizeof(char));
-
-	if (buffer == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		buffer[i] = str[i];
-
-	buffer[i] = '\0';
-
-	return (buffer);
-}
-/**
  * init_dog - initialise dog
  * @d: acceder a la struct
  * @name: ddd
@@ -57,15 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL || name == NULL || owner == NULL)
 		return;
 
-	d->name = _strdup(name);
-	d->owner = _strdup(owner);
-	if (d->name == NULL || d->owner == NULL)
-	{
-		free(d->name);
-		free(d->owner);
-		d->name = NULL;
-		d->owner = NULL;
-		return;
-	}
+	d->name = name;
+	d->owner = owner;
 	d->age = age;
 }
